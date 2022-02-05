d459514d922292defd0c1ded5268821574be692c
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-02-05 17:20:31 +0530

    Update BestTimeToBuyStock_I.cpp

--- LeetcodeDailyChallengeFebruary2022/day1/BestTimeToBuyStock_I.cpp
+++ LeetcodeDailyChallengeFebruary2022/day1/BestTimeToBuyStock_I.cpp
@@ -4,7 +4,7 @@ public
         int n=prices.size();
         int temp=prices[0];
         int profit=0;
-        for(int i=1;in;i++){
+        for(int i=1;i<n;i++){
             temp=min(temp,prices[i]);
             profit=max(profit,prices[i]-temp);
         }
