1d86751130fa8a6700ec17665fa3430931d43a53
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-03-29 08:10:23 +0530

    update 297

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,11 +1,15 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        #tortoise runs one step at a time
+        #hare runs two steps at a time
+        #since there are duplicates so hare and tortoise should intersected at some point. but this doesn't guarantee the entry point of the cycle.
         tortoise=hare=nums[0]
         flag=True
         while tortoise!=hare or  flag==True:
             tortoise=nums[tortoise]
             hare=nums[nums[hare]]
             flag=False
+        #usedto find the entry point of the cycle    
         tortoise=nums[0]
         while tortoise!=hare:
             tortoise=nums[tortoise]
