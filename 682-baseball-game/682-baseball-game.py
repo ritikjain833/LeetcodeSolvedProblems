d458fbc02eb0bb4222ff8e2f549bb6089716e845
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-04-10 07:26:24 +0530

    Time: 49 ms (70.47%), Space: 14.1 MB (74.69%) - LeetHub

--- 682-baseball-game/682-baseball-game.py
+++ 682-baseball-game/682-baseball-game.py
@@ -1,25 +1,25 @@
 from collections import deque
 class Solution:
     def calPoints(self, ops: List[str]) -> int:
-        score=0
+        #score=0
         val=deque()
         for aux in ops:
             if aux=="+":
                 temp=(int(val[-1])+int(val[-2]))
-                score+=temp
+                #score+=temp
                 val.append(temp)
             elif aux=="C":
                 temp=val.pop()
-                score-=temp
+                #score-=temp
             
             elif aux=="D":
                 val.append(int(val[-1])*2)
-                score+=int(val[-1])
+                #score+=int(val[-1])
             else:
                 val.append(int(aux))
-                score+=int(val[-1])
+                #score+=int(val[-1])
             #print(score)
             #print(val)
             
             
-        return score
+        return sum(val)
