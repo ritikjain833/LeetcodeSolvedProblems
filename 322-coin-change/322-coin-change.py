625ac4fabbbaa0d9fde6dbb3e0491ac859328c49
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-05-28 11:47:17 +0530

    Time: 3133 ms (10.11%), Space: 16.1 MB (27.27%) - LeetHub

---
+++ 322-coin-change/322-coin-change.py
@@ -0,0 +1,18 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        n=len(coins)
+        dp=[[10**4+1]*(amount+1) for _ in range(n+1)]
+        for i in range(1,n+1):
+            dp[i][0]=0
+        for i in range(1,n+1):
+            for j in range(1,amount+1):
+                if coins[i-1]<=j:
+                    dp[i][j]=min(dp[i-1][j],1+dp[i][j-coins[i-1]])
+                else:
+                    dp[i][j]=dp[i-1][j]
+        #print(dp)            
+        if dp[n][amount]!=(10**4+1):
+            return dp[n][amount]
+        else:
+            return -1
+        
