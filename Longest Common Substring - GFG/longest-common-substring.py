cf91d61fc38d5977aaafc801a468a3fff1d3c19e
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-05-28 15:15:07 +0530

    Added solution - LeetHub

---
+++ Longest Common Substring - GFG/longest-common-substring.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+
+class Solution:
+    def longestCommonSubstr(self, S1, S2, n, m):
+        # code here
+        dp=[[0]*(m+1) for _ in range(n+1)]
+        result=0
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if S1[i-1]==S2[j-1]:
+                    dp[i][j]=1+dp[i-1][j-1]
+                    result=max(result,dp[i][j])
+                else:
+                    dp[i][j]=0
+        #print(dp)            
+        return result
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        n,m = input().strip().split(" ")
+        n,m = int(n), int(m)
+        S1 = input().strip()
+        S2 = input().strip()
+        
+        
+        ob=Solution()
+        print(ob.longestCommonSubstr(S1, S2, n, m))
+# } Driver Code Ends
