991de7c4f8aed3224530c581addc49d5e07d8048
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-05-28 11:48:34 +0530

    Time: 2135 ms (40.25%), Space: 16.1 MB (26.38%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -2,7 +2,7 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n=len(coins)
         dp=[[10**4+1]*(amount+1) for _ in range(n+1)]
-        for i in range(1,n+1):
+        for i in range(0,n+1):
             dp[i][0]=0
         for i in range(1,n+1):
             for j in range(1,amount+1):
