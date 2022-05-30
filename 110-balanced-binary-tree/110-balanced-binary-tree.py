145b00752a6a51fdd4dfa8f16b59692f1371a982
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-05-30 15:44:09 +0530

    Time: 89 ms (28.12%), Space: 18.9 MB (11.37%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -4,13 +4,17 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
+#Time complexity is O(n)
+#One whole traversal is required.
+#space complexity is O(n)
 class Solution:
     def dfs(self,root):
         if root:
             left=self.dfs(root.left)
             right=self.dfs(root.right)
             balanced=left[0] and right[0] and abs(left[1]-right[1])<=1
+            if balanced==False:
+                return [False]
             return  [balanced,1+max(left[1],right[1])]
         else:
             return [True,0]
