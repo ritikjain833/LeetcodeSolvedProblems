6a364f396846d327eb75b6c1f3de7034a2ada0d8
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-06-02 08:20:38 +0530

    Time: 17 ms (42.74%), Space: 10.5 MB (95.40%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -4,8 +4,8 @@ public:
         int m=matrix.size();
         int n=matrix[0].size();
         vector<vector<int>> ans(n,vector<int>(m,0));
-        for(int j=0;j<n;j++){
-            for(int i=0;i<m;i++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 ans[j][i]=matrix[i][j];
             }
         }
