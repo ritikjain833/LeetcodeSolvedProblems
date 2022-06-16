295c334b8fd1f20f1dd3db90bd3cb3c156fec172
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-06-16 19:02:19 +0530

    Time: 1375 ms (5.23%), Space: 47.5 MB (27.87%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -8,11 +8,12 @@ public:
         for(int j=0;j<n;j++){
             for(int i=0;i<=j;i++){
                 bool flag=(s[i]==s[j]);
-                if (j-i>=2){
-                    dp[i][j]=dp[i+1][j-1] and flag;}
-                else{
+                if(i==j){dp[i][j]=flag;}
+                else if(j==i+1){
                     dp[i][j]=flag;
- 
+                }
+                else{
+                    dp[i][j]=dp[i+1][j-1] and flag;
                 }
                 if (dp[i][j] and j-i+1>max1){
                     max1=j-i+1;
