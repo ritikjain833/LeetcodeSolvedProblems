0e7dd60183fe43ed713ec3653b3e8c9650c08664
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-06-19 14:28:01 +0530

    Time: 123 ms (77.78%), Space: 17.1 MB (84.09%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,11 +1,14 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        '''Idea is to sort the products array and use the concept of two pointer approach'''
+        
         products.sort()
         res=[]
         left=0
         right=len(products)-1
         for i in range(len(searchWord)):
             char=searchWord[i]
+            #first check the condition and then check if sufficients characters are there in it or not product[left]/product[right] or if there is character mismatch then increase/decrease the corresponding pointer respectively.
             while left<=right and (len(products[left])<=i or products[left][i]!=char):
                 left+=1
             while left<=right and (len(products[right])<=i or products[right][i]!=char):
