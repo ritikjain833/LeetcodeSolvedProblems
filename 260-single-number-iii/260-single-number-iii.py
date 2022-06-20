b5affa6aa5a62f1702eb14c57d460ec38b086fb1
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-06-20 18:12:26 +0530

    Time: 146 ms (20.08%), Space: 15.7 MB (81.91%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.py
+++ 260-single-number-iii/260-single-number-iii.py
@@ -1,11 +1,35 @@
+# class Solution:
+#     def singleNumber(self, nums: List[int]) -> List[int]:
+#         dic=Counter(nums)
+#         ans=[]
+#         for i,j in dic.items():
+#             if j==1:
+#                 ans.append(i)
+#         return ans  
 class Solution:
-    def singleNumber(self, nums: List[int]) -> List[int]:
-        dic=Counter(nums)
-        ans=[]
-        for i,j in dic.items():
-            if j==1:
-                ans.append(i)
-        return ans        
+    
+    def singleNumber(self,nums: List[int]) -> List[int]:
+        xor1=0
+        xor2=0
+        index=0
+        """finding the xor of all elements will result in xor of two unique elements"""
+        for i in nums:
+            xor1=xor1^i
+        # Now for finding the one number we can find the first set bit from the lsb .
+        #other will have zero corresponding bit .
+        # now xoring all the elements at the corresponding index will result in cancellation of all numbers except one single number that is unique.
+        #  find xor with total u will get second element.
+        for i in range(32):
+            if (xor1&(1<<i)):
+                index=i
+                break
+        for n in nums:
+            if(n&(1<<i)):
+                xor2=xor2^n
+        return [xor2,xor1^xor2]        
+                       
+        
+
         
       
         
