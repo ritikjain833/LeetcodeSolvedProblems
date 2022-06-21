d1ea6c64ef65f75f2f38617d7bdebdc1b34c11ea
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-06-21 16:14:26 +0530

    Time: 651 ms (90.08%), Space: 208.5 MB (31.26%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,5 +1,48 @@
 class Solution {
 public:
+//     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+//         vector<int> adj[n];
+//         int m=edges.size();
+//         for(int i=0;i<m;i++){
+//             adj[edges[i][0]].push_back(edges[i][1]);
+//             adj[edges[i][1]].push_back(edges[i][0]);
+//         }
+//         queue<int> q;
+//         q.push(source);
+//         vector<bool> visited(n,false);
+//         visited[source]=true;
+//         while (!q.empty()){
+//             int aux=q.front();
+//             q.pop();
+//             if(aux==destination){
+//                 return true;
+//             }
+//             for(auto out: adj[aux]){
+//                 //cout<<out;
+//                 if(visited[out]==false){
+//                     q.push(out);
+//                     visited[out]=true;
+//                 }
+                
+//             }
+//         }
+//         return false;
+        
+//     }
+    bool dfs(int source,vector<int> adj[],int destination,vector<bool> & visited){
+        if (source==destination){
+            return true;
+        }
+        visited[source]=true;
+        for(auto out: adj[source]){
+            if(visited[out]==false){
+                if (dfs(out,adj,destination,visited)==true){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         vector<int> adj[n];
         int m=edges.size();
@@ -7,26 +50,7 @@ public:
             adj[edges[i][0]].push_back(edges[i][1]);
             adj[edges[i][1]].push_back(edges[i][0]);
         }
-        queue<int> q;
-        q.push(source);
-        vector<bool> visited(n,false);
-        visited[source]=true;
-        while (!q.empty()){
-            int aux=q.front();
-            q.pop();
-            if(aux==destination){
-                return true;
-            }
-            for(auto out: adj[aux]){
-                //cout<<out;
-                if(visited[out]==false){
-                    q.push(out);
-                    visited[out]=true;
-                }
-                
-            }
-        }
-        return false;
-        
+        vector<bool>visited(n,false);
+        return dfs(source,adj,destination,visited);
     }
 };
