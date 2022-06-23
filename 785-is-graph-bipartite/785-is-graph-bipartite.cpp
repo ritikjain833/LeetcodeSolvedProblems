0c108930c5aac5aafc98aab1edf026f439268e1c
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-06-23 21:45:34 +0530

    Time: 38 ms (37.92%), Space: 13.8 MB (27.37%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -13,20 +13,43 @@ public:
         }
         return true;
     }
+     bool bfs(int src,vector<vector<int>> &adj,vector<int> &color){
+        queue<int> q;
+        q.push(src);
+        color[src]=1;
+        while(q.size()!=0){
+            int parent=q.front();
+            q.pop();
+            for(int i=0;i<adj[parent].size();i++){
+                int child=adj[parent][i];
+                if(color[child]==-1){
+                    q.push(child);
+                    color[child]=1-color[parent];
+                }
+                else if(color[child]==color[parent]){
+                    return false;
+                    
+                }
+                
+            }
+        }
+        return true;
+        
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         int V=graph.size();
         
          vector<int> color(V,-1);
 	    for(int i=0;i<V;i++){
 	        if (color[i]==-1){
-	        //bool ans=bfs(i,adj,color);
-	        //if (ans==false)return false;}
-	        color[i]=1;
-	        if (dfs(i,graph,color)==false)return false;
+	        bool ans=bfs(i,graph,color);
+	        if (ans==false)return false;}
+	        // color[i]=1;
+	        // if (dfs(i,graph,color)==false)return false;
 	    }
 	    
-	    
-	}return true;
+	  return true;  
+	
         
     }
 };
