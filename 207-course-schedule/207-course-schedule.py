476f61e36e337a9bc554d508cb253cc1b0d63247
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-06-23 10:28:48 +0530

    Time: 229 ms (10.92%), Space: 15.3 MB (96.50%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,21 +1,26 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        """First step graph construction"""
         graph=defaultdict(list)
-        n=numCourses
-        degree=[0]*(n)
-        for j,i in prerequisites:
-            graph[i].append(j)
-            degree[j]+=1
+        # maintain degree
+        degree=[0]*(numCourses)
+        for i,j in prerequisites:
+            graph[j].append(i)
+            degree[i]+=1
+        """BFS approach using indegree"""
         queue=deque()
-        for i in range(n):
+        for i in range(numCourses):
             if degree[i]==0:
                 queue.append(i)
         while queue:
-            aux=queue.popleft()
-            for out in graph[aux]:
+            front=queue.popleft()
+            for out in graph[front]:
                 degree[out]-=1
                 if degree[out]==0:
                     queue.append(out)
         if sum(degree)==0:
             return True
-        return False
+        else:
+            return False
+            
+        
