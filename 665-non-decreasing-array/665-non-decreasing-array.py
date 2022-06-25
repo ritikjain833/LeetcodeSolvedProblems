dffdaba6cd0820d82d649693caf458a12caee10c
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-06-25 14:44:23 +0530

    Time: 201 ms (87.23%), Space: 15.2 MB (91.17%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.py
+++ 665-non-decreasing-array/665-non-decreasing-array.py
@@ -3,13 +3,15 @@ class Solution:
         count=0
         if len(nums) in (1,2):
             return True
-        for i in range(1,len(nums)):
-            if nums[i-1]>nums[i]:
-                if(count==1):
-                    return False
-                if i-2>=0 and i+1<len(nums) and nums[i-2]>nums[i] and nums[i-1]>nums[i+1]:
-                    return False
-                count+=1
-            
-                
-        return True
+        flag=False
+        for i in range(len(nums)-1):
+            if nums[i]<=nums[i+1]:
+                continue
+            if flag:
+                return False
+            if i==0 or nums[i+1]>=nums[i-1]:
+                nums[i]=nums[i+1]
+            else:
+                nums[i+1]=nums[i]
+            flag=True    
+        return True   
