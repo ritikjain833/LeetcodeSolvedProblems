2be7768aa72f8f1323af5803e390a9af2455af01
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-06-25 13:58:52 +0530

    Added solution - LeetHub

--- Fill the Tank - GFG/fill-the-tank.py
+++ Fill the Tank - GFG/fill-the-tank.py
@@ -6,13 +6,17 @@ sys.setrecursionlimit(10**7)
 class Solution:
     def dfs(self,S,adj,Cap,target,parent=None):
         target-=Cap[S-1]
+        # if target is less than zero we cannot fill in that case.
         if target<0:
             return False
-        n=len(adj[S])    
+        n=len(adj[S])   
+        # leaving the source node
         if parent:
             n=n-1
+        #leaf node termination    
         if n==0:
             return True
+        #finding the remaining water    
         dist=target//n
         for out in adj[S]:
             if out==parent:
@@ -21,6 +25,32 @@ class Solution:
                 return False
         return True        
         #n=len(adj[S])
+    def bfs(self,S,adj,Cap,target):
+        visited=[False]*(N+1)
+        visited[S]=True
+        queue=deque()
+        queue.append((S,target))
+        while queue:
+            aux,remain=queue.popleft()
+            remain-=Cap[aux-1]
+            #print(aux,remain)
+            if remain<0:
+                #print("I am here")
+                return False
+            n=len(adj[aux])
+            if aux!=S:    
+                n=n-1  
+            if n==0:
+                continue
+            left=remain//n
+            for out in adj[aux]:
+                if aux==out:
+                    continue
+                if visited[out]==False:
+                    queue.append((out,left))
+                    #print(out,left)
+                    visited[out]=True
+        return True    
         
                     
                 
@@ -34,13 +64,17 @@ class Solution:
             adj[j].append(i)
         """DFS approach"""
         #C=[0]+Cap
-        low=1
+        #minimum Cap required is Sum(Cap)
+        low=sum(Cap)
         high=10**18+1
         while low<high:
             mid=low+(high-low)//2
             ans=self.dfs(S,adj,Cap,mid)
+            #print(mid,ans)
+            # if ans is possible atcan be upto this so use high=mid
             if ans:
                 high=mid
+            #if ans is not possible just increase the low=mid+1    
             else:
                 low=mid+1
         if low>10**18:
