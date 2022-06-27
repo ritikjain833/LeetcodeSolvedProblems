abfd1352f92a6cfde60060ef303ed31e29f94a9f
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-06-27 18:39:42 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.py
+++ Topological sort - GFG/topological-sort.py
@@ -1,21 +1,50 @@
+from collections import deque
 class Solution:
+    """DFS approach is used to finding the ordering of topological sort"""
     def dfs(self,i,adj,ans,visited):
         visited[i]=True
+        #ans.append(i)
         for out in adj[i]:
             if visited[out]==False:
                 self.dfs(out,adj,ans,visited)
+               
         ans.append(i)        
-                
         
     #Function to return list containing vertices in Topological order.
     def topoSort(self, V, adj):
         # Code here
-        visited=[False]*(V)
-        ans=[]
+        # visited=[False]*(V)
+        # ans=[]
+        # for i in range(V):
+        #     if visited[i]==False:
+        #         self.dfs(i,adj,ans,visited)
+        # return ans[::-1] 
+        """Kahn's algorithm to find topological sort"""
+        degree=[0]*(V)
         for i in range(V):
-            if visited[i]==False:
-                self.dfs(i,adj,ans,visited)
-        return ans[::-1]        
+            for j in adj[i]:
+                degree[j]+=1
+        queue=deque()
+        ans=[]
+        visited=[False]*(V)
+        for i in range(V):
+            if degree[i]==0:
+                queue.append(i)
+                ans.append(i)
+                visited[i]=True
+                
+        while queue:
+            aux=queue.popleft()
+            for out in adj[aux]:
+                if visited[out]==False:
+                    degree[out]-=1
+                    if degree[out]==0:
+                        queue.append(out)
+                        ans.append(out)
+        return ans                
+                        
+                        
+                
 
 
 #{ 
