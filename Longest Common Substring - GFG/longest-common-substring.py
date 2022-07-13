4e9a28e84bc41d7cd51a2f109b90f3d696c9e448
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-07-13 16:29:15 +0530

    Added solution - LeetHub

--- Longest Common Substring - GFG/longest-common-substring.py
+++ Longest Common Substring - GFG/longest-common-substring.py
@@ -4,16 +4,15 @@ class Solution:
     def longestCommonSubstr(self, S1, S2, n, m):
         # code here
         dp=[[0]*(m+1) for _ in range(n+1)]
-        result=0
+        res=0
         for i in range(1,n+1):
             for j in range(1,m+1):
                 if S1[i-1]==S2[j-1]:
                     dp[i][j]=1+dp[i-1][j-1]
-                    result=max(result,dp[i][j])
+                    res=max(res,dp[i][j])
                 else:
                     dp[i][j]=0
-        #print(dp)            
-        return result
+        return res            
 
 #{ 
 #  Driver Code Starts
