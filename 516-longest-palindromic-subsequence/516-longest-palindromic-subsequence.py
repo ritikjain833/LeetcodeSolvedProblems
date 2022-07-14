8aaeed53d0bc02c917cc813d1c74065e24d74cae
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-07-14 21:25:39 +0530

    Time: 3839 ms (30.33%), Space: 39.4 MB (38.41%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
@@ -9,5 +9,17 @@ class Solution:
                     dp[i][j]=1+dp[i-1][j-1]
                 else:
                     dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-        return dp[n][n]            
-        
+        i=n
+        j=n
+        ans=[]
+        while i>0 and j>0:
+            if s[i-1]==t[j-1]:
+                ans.append(s[i-1])
+                i-=1
+                j-=1
+            elif dp[i-1][j]>dp[i][j-1]:
+                i-=1
+            else:
+                j-=1
+        #print("".join(ans))
+        return dp[n][n]
