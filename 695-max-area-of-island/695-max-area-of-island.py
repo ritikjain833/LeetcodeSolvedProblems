68c83a398082a446671ee6d53506daa662c2f163
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-07-15 10:01:35 +0530

    Time: 149 ms (88.55%), Space: 16.5 MB (58.06%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,19 +1,19 @@
 class Solution:
-    def dfs(self,grid,r,c,m,n,visited):
-        if r<0 or r>=m or c<0 or c>=n or grid[r][c]==0 or visited[r][c]==True:
+    def dfs(self,grid,r,c,m,n):
+        if r<0 or r>=m or c<0 or c>=n or grid[r][c]==0 :
             return 0
-        visited[r][c]=True
-        return 1+self.dfs(grid,r,c+1,m,n,visited)+self.dfs(grid,r,c-1,m,n,visited)+self.dfs(grid,r+1,c,m,n,visited)+self.dfs(grid,r-1,c,m,n,visited)
+        grid[r][c]=0
+        return 1+self.dfs(grid,r,c+1,m,n)+self.dfs(grid,r,c-1,m,n)+self.dfs(grid,r+1,c,m,n)+self.dfs(grid,r-1,c,m,n)
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         m=len(grid)
         n=len(grid[0])
-        visited=[[False]*(n) for _ in range(m)]
+        #visited=[[False]*(n) for _ in range(m)]
         area=0
         for i in range(m):
             for j in range(n):
                 
                 if grid[i][j]==1:
-                    area=max(area,self.dfs(grid,i,j,m,n,visited))
+                    area=max(area,self.dfs(grid,i,j,m,n))
         return area            
                 
                 
