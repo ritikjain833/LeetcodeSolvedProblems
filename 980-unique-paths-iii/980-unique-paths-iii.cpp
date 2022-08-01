861438981696bb5c6a532fde176d031671b77a3d
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-08-01 14:27:33 +0530

    Time: 6 ms (49.98%), Space: 7 MB (65.24%) - LeetHub

--- 980-unique-paths-iii/980-unique-paths-iii.cpp
+++ 980-unique-paths-iii/980-unique-paths-iii.cpp
@@ -3,8 +3,10 @@ public:
     int res=0;
     int sx,sy;
     int m,n;
-    //int dr[4]={-1,1,0,0};
-    //int dc[4]={0,0,-1,1};
+    int dr[4]={-1,1,0,0};
+    int dc[4]={0,0,-1,1};
+    //starting one should also be counted as empty.
+    //consider the example [[1,2]] here ans is one. and there is no empty boxes.
     int empty=1;
     void dfs(vector<vector<int>> &grid,int x,int y){
         if(x<0|| x>=m || y<0 || y>=n || grid[x][y]<0){
@@ -19,10 +21,14 @@ public:
         }
         grid[x][y]=-2;
         empty--;
-        dfs(grid,x+1,y);
-        dfs(grid,x-1,y);
-        dfs(grid,x,y-1);
-        dfs(grid,x,y+1);
+        // dfs(grid,x+1,y);
+        // dfs(grid,x-1,y);
+        // dfs(grid,x,y-1);
+        
+        // dfs(grid,x,y+1);
+        for(int i=0;i<4;i++){
+            dfs(grid,x+dr[i],y+dc[i]);
+        }
         empty++;
         grid[x][y]=0;
         return ;
