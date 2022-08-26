4c15eb08d5d26eddf724b5d3cd07f83264e85de2
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-08-26 09:53:31 +0530

    Time: 2 ms (79.10%), Space: 5.9 MB (83.90%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
+    
+    //function to convert the number to string and store them in sorted order 
     string sort_digits(int n){
         string digits=to_string(n);
         sort(digits.begin(),digits.end());
         return digits;
     }
     bool reorderedPowerOf2(int n) {
+        //converted string 
         string target=sort_digits(n);
         for(int i=0;i<31;i++){
+            //check for each number i.e. of two. and convert into string and check with target
             string res=sort_digits(1<<i);
+            //if found return true
             if(res==target)return true;
             
         }
+        //not found
+        
         return false;
         
     }
