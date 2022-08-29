a80134d4376d1558ac890256febcd9e9492857be
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-08-30 00:49:29 +0530

    Time: 321 ms (39.88%), Space: 139.7 MB (9.87%) - LeetHub

---
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int dx[4]={-1,0,1,0};
+    int dy[4]={0,1,0,-1};
+    int m;
+    int n;
+    vector<vector<int>> ans;
+    void bfs(vector<vector<int>>& land,int sr,int sc){
+        int dr=-1;
+        int dc=-1;
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        land[sr][sc]=0;
+        while(!q.empty()){
+            pair<int,int> t=q.front();
+            q.pop();
+            dr=t.first;
+            dc=t.second;
+            for(int i=0;i<4;i++){
+                int nr=dr+dx[i];
+                int nc=dc+dy[i];
+                if(nr>=0 and nr<m and nc>=0 and nc<n and land[nr][nc]==1){
+                    land[nr][nc]=0;
+                    q.push({nr,nc});
+                    
+                }
+            }
+        }
+        ans.push_back({sr,sc,dr,dc});
+    }
+    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+        //use bfs approach
+        m=land.size();
+        n=land[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(land[i][j]==1){
+                    bfs(land,i,j);
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
