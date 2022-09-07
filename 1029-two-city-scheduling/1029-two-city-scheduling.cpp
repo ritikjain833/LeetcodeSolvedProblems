fa10c9f131df778256da12919b45240e137a605c
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-09-07 17:27:18 +0530

    Time: 4 ms (82.71%), Space: 7.8 MB (95.45%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        int sum1=0;
-        vector<int> ans;
-        for(auto x:costs){
-            sum1+=x[1];
-            ans.push_back(x[0]-x[1]);
-        }
+        int sum=0;
         int n=costs.size();
-        sort(ans.begin(),ans.end());
-        for(int i=0;i<n/2;i++)
-        {
-            sum1+=ans[i];
+        vector<int> diff;
+        for(int i=0;i<n;i++){
+            sum+=costs[i][1];
+            diff.push_back(costs[i][0]-costs[i][1]);
+            
+            
         }
-        return sum1;
-        
+        sort(diff.begin(),diff.end());
+        for(int i=0;i<n/2;i++){
+            sum+=diff[i];
+            
+        }
+        return sum;
         
     }
 };
