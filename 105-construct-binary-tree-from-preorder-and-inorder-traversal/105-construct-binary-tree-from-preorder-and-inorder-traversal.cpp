63fbe093d0c884d454ea6e186a5baddeb9ad950f
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-09-10 19:32:06 +0530

    Time: 13 ms (96.32%), Space: 26.4 MB (43.47%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,16 +11,22 @@
  */
 class Solution {
 public:
+    //root element always pointed by index count
     int index=0;
+    //mp is used to create a inorder[i] mapping to index based mapping
     unordered_map<int,int> mp;
     TreeNode* solve(vector<int> &preorder,int left,int right){
         if(left>right){
             return NULL;
         }
+        //find the rootval
         int root_val=preorder[index++];
         //cout<<root_val<<endl;
+        //Create the root node
         TreeNode* root=new TreeNode(root_val);
+        //recurse over the left subtree
         root->left=solve(preorder,left,mp[root_val]-1);
+        //recurse over the right subtree 
         root->right=solve(preorder,mp[root_val]+1,right);
         return root;
     }
