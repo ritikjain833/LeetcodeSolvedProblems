8cd88eaeee682064f7d15b5ae293f95c9d59cb34
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-09-12 11:51:39 +0530

    Time: 120 ms (13.21%), Space: 14 MB (77.36%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.py
+++ 948-bag-of-tokens/948-bag-of-tokens.py
@@ -1,21 +1,29 @@
 class Solution:
     def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        #sort tokens in ascending order
         tokens.sort()
+        #current_count
         count=0
         ans=0
+        #use two pointer approach to solve this problem
         i=0
         j=len(tokens)-1
+        #if current_power is more than tokens[i] it is greedier to collect that and move forware
+        
         while i<=j:
             if power>=tokens[i]:
                 count+=1
                 ans=max(count,ans)
                 power-=tokens[i]
                 i+=1
+            #if not then we have two cases when count>=1 we can decrease and shrink the window or we can't do anything and just return ans    
             else:
                 if count>=1:
                     count-=1
                     power+=tokens[j]
-                j-=1
+                    j-=1
+                else:
+                    return ans
                 
            
         return ans        
