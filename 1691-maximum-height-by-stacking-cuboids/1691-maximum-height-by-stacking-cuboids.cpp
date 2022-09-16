5c4161b113bf613048073475ef9bd03f8e62b5b8
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-09-16 21:44:48 +0530

    Time: 25 ms (56.84%), Space: 9.1 MB (71.32%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
@@ -10,8 +10,9 @@ public:
         int n=cuboids.size();
         int res=0;
         vector<int> dp(n,0);
-        dp[0]=cuboids[0][2];
+        //dp[0]=cuboids[0][2];
         for(int j=1;j<n;j++){
+            dp[j]=cuboids[j][2];
             for(int i=0;i<j;i++){
                 if(cuboids[i][0]<=cuboids[j][0] and cuboids[i][1]<=cuboids[j][1] and cuboids[i][2]<=cuboids[j][2]){
                     dp[j]=max(dp[j],dp[i]+cuboids[j][2]);
