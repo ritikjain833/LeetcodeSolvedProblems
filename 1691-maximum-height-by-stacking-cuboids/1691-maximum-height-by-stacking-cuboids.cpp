f11e645913a95afc7fb5399531e068232576c1a3
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-09-16 22:26:09 +0530

    Time: 29 ms (44.27%), Space: 9.2 MB (71.32%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
@@ -5,20 +5,23 @@ public:
             sort(a.begin(),a.end());
             
         }
-        cuboids.push_back({0,0,0});
+    //cuboids.push_back({0,0,0});
         sort(cuboids.begin(),cuboids.end());
         int n=cuboids.size();
         int res=0;
         vector<int> dp(n,0);
         //dp[0]=cuboids[0][2];
-        for(int j=1;j<n;j++){
-            dp[j]=cuboids[j][2];
-            for(int i=0;i<j;i++){
-                if(cuboids[i][0]<=cuboids[j][0] and cuboids[i][1]<=cuboids[j][1] and cuboids[i][2]<=cuboids[j][2]){
-                    dp[j]=max(dp[j],dp[i]+cuboids[j][2]);
-                    res=max(res,dp[j]);
+        for(int i=0;i<n;i++){
+            dp[i]=cuboids[i][2];
+            for(int j=0;j<i;j++){
+                if(cuboids[j][0]<=cuboids[i][0] and cuboids[j][1]<=cuboids[i][1] and cuboids[j][2]<=cuboids[i][2]){
+                    if(dp[i]<dp[j]+cuboids[i][2]){
+                        dp[i]=dp[j]+cuboids[i][2];
+                    }
                 }
+                
             }
+            res=max(res,dp[i]);
         }
         return res;
         
