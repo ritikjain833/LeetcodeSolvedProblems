7a544659da694e1b1092b3570d4ea5bf723b2c83
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-09-21 10:23:55 +0530

    Time: 187 ms (7.42%), Space: 44.2 MB (14.47%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -3,16 +3,19 @@ public:
     int maxScore(vector<int>& cardPoints, int k) {      
         //Instead of finding the maximum score we can find the consective minimum score (subarray) using the concept of prefix sum
         //answer will be total_sum-mini;
-        int mini=INT_MAX;
+        int mini=INT_MIN;
         int n=cardPoints.size();
         vector<int> prefix(n+1,0);
         for(int i=0;i<n;i++){
             prefix[i+1]=prefix[i]+cardPoints[i];
         }
+        int total=prefix[n];
         for(int i=0;i<=k;i++){
-            mini=min(mini,prefix[i+n-k]-prefix[i]);
+            mini=max(mini,total-(prefix[i+n-k]-prefix[i]));
+            //mini=min(mini,prefix[i+n-k]-prefix[i])
         }
-        return prefix[n]-mini;
+        //return total-mini;
+        return mini;
         
     }
 };
