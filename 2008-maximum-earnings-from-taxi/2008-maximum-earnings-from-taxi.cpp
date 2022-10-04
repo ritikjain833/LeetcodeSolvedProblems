85ed8cc81dc382e8c03569c8fd41bebad67a9d14
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-10-04 23:23:36 +0530

    Time: 1154 ms (39.22%), Space: 104.8 MB (57.52%) - LeetHub

--- 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    long long maxTaxiEarnings(int n, vector<vector<int>>& rides) {
-        
-        //use the concept of dynamic programming here.
-        //dp[i] := max dollars you can earn ending at i
-        vector<vector<pair<int,int>>> vp(n+1);
-        vector<long long> dp(n+1,0);
-        for(auto x:rides){
-            vp[x[1]].push_back({x[0],x[1]-x[0]+x[2]});
-        }
-        for(int i=1;i<=n;i++){
-            //if not ending found at that index
-            //we can choose the previous value
-            dp[i]=dp[i-1];
-            for(auto x:vp[i]){
-                dp[i]=max(dp[i],dp[x.first]+x.second);
-                
-            }
-        }
-        return dp[n];
-        
-        
-    }
+	long long f(int i,vector<vector<int>>& r, vector<int>& st,int m,vector<long long>& dp){
+		if(i>=m) return 0;
+		if(dp[i]!=-1) return dp[i];
+        // Searching for next passenger who can sit  in the car
+		int ind=lower_bound(st.begin(),st.end(),r[i][1])-st.begin();
+		long long pick=(r[i][1]-r[i][0]+r[i][2])+f(ind,r,st,m,dp);
+		long long notpick=f(i+1,r,st,m,dp);
+		return dp[i]=max(pick,notpick);
+	}
+
+	long long maxTaxiEarnings(int n,vector<vector<int>>& r) {
+		int m=r.size();
+		sort(r.begin(),r.end());
+		vector<int> st;
+		for(int i=0;i<m;i++) st.push_back(r[i][0]);
+		vector<long long> dp(m,-1);
+		return f(0,r,st,m,dp);
+	}
 };
