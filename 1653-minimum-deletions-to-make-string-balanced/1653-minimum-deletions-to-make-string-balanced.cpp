d1f6405af3e5cc9b8513e19f846da87344232dc7
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2022-11-23 09:02:06 +0530

    Time: 281 ms (42.56%), Space: 23 MB (46.43%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
+       //Remove all occurences of ba
+        stack<char> st;
+        int count=0;
         int n=s.size();
-        vector<int> prefix(n+1,0);
-        for(int i=1;i<=n;i++){
-            prefix[i]=prefix[i-1];
-            if(s[i-1]=='b'){
-                prefix[i]++;
+        for(int i=0;i<n;i++){
+            if(!st.empty() and st.top()=='b' and s[i]=='a'){
+                count++;
+                st.pop();
             }
-    
+            else{
+                st.push(s[i]);
+}
         }
-        // for(int i=1;i<=n;i++){
-        //     cout<<prefix[i]<<endl;
-        // }
-        vector<int> suffix(n+1,0);
-        for(int i=n-1;i>=0;i--){
-            suffix[i]=suffix[i+1];
-            if(s[i]=='a')
-                suffix[i]++;
-        }
-        // for(int i=0;i<=n;i++){
-        //     cout<<suffix[i]<<endl;
-        // }
-        int ans=INT_MAX;
-        for(int i=0;i<=n;i++){
-            ans=min(ans,prefix[i]+suffix[i]);
-        }
-        return ans;
+        return count;
     }
 };
