cfb1074f040d763586e4c090db0bc3bb7ae32f55
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2023-01-19 11:48:43 +0530

    Time: 83 ms (48.78%), Space: 41.5 MB (13.48%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -4,12 +4,12 @@ public:
         int n=nums.size();
         vector<int> right_max(n);
         right_max[n-1]=nums[n-1];
-        int ss=nums[n-1];
+        int suffix_sum=nums[n-1];
         //here we have saved the right_max[i] at each index from i to last
         //using the concept of suffix_sum
         for(int i=n-2;i>=0;i--){
-            ss+=nums[i];
-            right_max[i]=max(right_max[i+1],ss);
+            suffix_sum+=nums[i];
+            right_max[i]=max(right_max[i+1],suffix_sum);
         }
         //max_sum stores the max_sum or the normal sum without circular array
         int max_sum=nums[0];
