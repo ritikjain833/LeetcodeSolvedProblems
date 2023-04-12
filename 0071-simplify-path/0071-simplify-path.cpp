3d79741f86382b4cd428c44e7a9638a15eeaff6c
Ritik Jain <39840433+ritikjain833@users.noreply.github.com>
2023-04-12 15:32:53 +0530

    Time: 3 ms (92.26%), Space: 10.6 MB (46.42%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        
+        stack<string> st;
+        string res;
+        int n=path.size();
+        for(int i=0;i<n;i++){
+            if(path[i]=='/') continue;
+            string temp;
+            while(i<n and path[i]!='/'){
+                temp+=path[i];
+                i++;
+            }
+            //cout<<temp<<endl;
+            if(temp=="."){
+                continue;
+                
+            }
+            if(temp==".."){
+                if(!st.empty()){
+                    st.pop();
+                }
+
+            }
+            else{
+                    st.push(temp);
+                }
+        }
+        while(!st.empty()){
+            res="/"+st.top()+res;
+            st.pop();
+        }
+        if(res.size()==0){
+            return "/";
+        }
+        return res;
+    }
+};
